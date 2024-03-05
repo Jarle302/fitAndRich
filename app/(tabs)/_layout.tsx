8a440b60fee35f97aa137a8901f062362a5a5f4b
Jarle302 <jarlehtollaksen2@live.no>
2024-03-05 09:09:07 +0100

    add structure home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs, } from "expo-router";
-import { Pressable } from "react-native";
+import { Link, Tabs } from "expo-router";
+import { ImageBackground, Pressable } from "react-native";
 
 export default function _layout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{title:"Home"}} />
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+    
     </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,95 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Pressable, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Pressable,
+  Modal,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+import meals from "../../constants/mockData";
+
 
 export default function home() {
   const [modalVisible, setModalVisible] = useState(false);
-
+  const [text, setText] = useState("");
   return (
     <>
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.title}>Streak</Text>
+      <ImageBackground
+        source={require("/home/jarledev/github/expo/fitAndRich/assets/images/appBackground.png")}
+        style={{ height: "100%" }}
+        blurRadius={0}>
+        <View style={styles.container}>
+          {/* <View>
+         <Text style={{...styles.title,color:"red",backgroundColor:"white"}}>Streak</Text>
         </View>
+*/}
 
-        <Pressable
-          style={styles.button}
-          onPress={() => setModalVisible((prev) => !prev)}
-          onLongPress={() => alert("this is a long press")}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Motivate Me</Text>
-        </Pressable>
-
-        <View style={styles.caloriesContainer}>
-          <View style={styles.textContainer}>
-            <Text style={styles.smallerText}>Remaining</Text>
-            <Text style={styles.text}>540 cal</Text>
+          <View style={styles.caloriesContainer}>
+            <View style={styles.textContainer}>
+              <Text style={styles.smallerText}>Remaining</Text>
+              <Text style={styles.text}>540 cal</Text>
+            </View>
+            <Text style={styles.resetText}>Resets in: 542</Text>
           </View>
-          <Text style={styles.resetText}>Resets in: 542</Text>
+
+          <View style={{ gap: 8,backgroundColor:"#8BA493",borderRadius:12,padding:10, }}>
+            <TextInput
+              style={{
+                width: 235,
+                padding: 15,
+                backgroundColor: "white",
+                color: "black",
+                borderRadius: 12,
+                fontWeight: "bold",
+              }}
+              onChangeText={setText}
+              value={text}
+              placeholder="What did you eat?"
+            />
+            <TextInput
+              inputMode="numeric"
+              style={{
+                width: 235,
+                padding: 15,
+                backgroundColor: "white",
+                color: "black",
+                borderRadius: 12,
+                fontWeight: "bold",
+              }}
+              onChangeText={setText}
+              value={text}
+              placeholder="Number of Calories"
+            />
+              <Pressable
+                style={styles.button}
+                onPress={() => setModalVisible((prev) => !prev)}
+                onLongPress={() => alert("this is a long press")}>
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                  Log Meal
+                </Text>
+              </Pressable>
+          </View>
         </View>
-        <View></View>
-      </View>
 
-      <Modal
-        style={styles.modal}
-        visible={modalVisible}
-        transparent={false}
-        animationType="slide">
-        
-        <Pressable
-          style={styles.button}
-          onPress={() => setModalVisible((prev) => !prev)}
-          onLongPress={() => alert("this is a long press")}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Motivate Me</Text>
-        </Pressable>
+        <Modal
+          style={styles.modal}
+          visible={modalVisible}
+          transparent={false}
+          animationType="slide">
+          <Pressable
+            style={styles.button}
+            onPress={() => setModalVisible((prev) => !prev)}
+            onLongPress={() => alert("this is a long press")}>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              Motivate Me
+            </Text>
+          </Pressable>
 
-        <Text>Modal</Text>
-
-      </Modal>
+          <Text>Modal</Text>
+        </Modal>
+      </ImageBackground>
     </>
   );
 }
@@ -95,7 +141,7 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     padding: 10,
     marginTop: 20,
-    width: 250,
+    width: 235,
     alignItems: "center",
   },
 });
